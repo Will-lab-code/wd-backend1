2aacb18c9ec0a47d390b8b748200a249ba3e1c32
Will Descarrega <wadescarrega@gmail.com>
2025-09-18 22:36:10 -0300

    Entrega 1 - Backend API REST

--- entrega1-backend/src/managers/ProductManager.js
+++ entrega1-backend/src/managers/ProductManager.js
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Soporte para __dirname en ESModules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
